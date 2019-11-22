acc8e314a740b51796db0dc27d6b0bcef83dac81
Daniel Wilder <dauncy@gmail.com>
2019-11-22 10:37:47 -0800

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,31 @@
+class CouponsController < ApplicationController
+  before_action :set_coupon, only: [:show] 
+    def index
+    @coupons = Coupon.all
+    end 
+
+  def new
+  end 
+
+  
+
+  def show
+
+  end 
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+   
+    redirect_to coupon_path(@coupon.id)
+  end 
+
+  
+
+  private
+  def set_coupon
+   @coupon = Coupon.find(params[:id])
+  end 
+end
